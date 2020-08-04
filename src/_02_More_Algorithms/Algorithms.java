d92e25510fdf12afcda3ebdb66d87ea661713c37
11002084 <36611325+11002084@users.noreply.github.com>
2020-08-04 16:35:29 -0700

    8/4/2020
    Finished More and More Tests, almost finished with Searching and Sorting Algorithms. I did the joke morse code translation thing, and would like to know what on earth message 2 is supposed to mean.

--- src/_02_More_Algorithms/Algorithms.java
+++ src/_02_More_Algorithms/Algorithms.java
@@ -1,9 +1,172 @@
 package _02_More_Algorithms;
 
+import java.util.Arrays;
 import java.util.List;
 
 public class Algorithms {
-	public static int findBrokenEgg(List<String> eggs) {
-		return 0;
+	
+	public static int findBrokenEgg(List<String> eggs) 	{
+	
+		for(int i=0; i<eggs.size(); i++) {
+			if(eggs.get(i) == "cracked") {
+				return i;
+			}
+		}
+		
+		return -1;
+	}
+	
+	public static int countPearls(List<Boolean> oysters) {
+		int numPearls = 0;
+		
+		for(boolean oyster : oysters) {
+			if(oyster) {
+				numPearls++;
+			}
+		}
+		
+		return numPearls;
+	}
+	
+	public static double findTallest(List<Double> peeps) {
+		double tallestHeight = -1;
+		
+		for(int i=0; i<peeps.size(); i++) {
+			if(peeps.get(i) > tallestHeight) {
+				tallestHeight = peeps.get(i);
+			}
+		}
+		
+		return tallestHeight;
+	}
+	
+	public static String findLongestWord(List<String> words) {
+		int index = -1;
+		int currLongestCharCount = -1;
+		
+		for(int i=0; i<words.size(); i++) {
+			if(words.get(i).length() > currLongestCharCount) {
+				index = i;
+				currLongestCharCount = words.get(i).length();
+			}
+		}
+		
+		return words.get(index);
+	}
+	
+	public static boolean containsSOS(List<String> message) {
+		for(int i=0; i<message.size(); i++) {
+			if(message.get(i).equals("... --- ...")){
+				return true;
+			}
+		}
+		
+		return false;
+	}
+	
+	public static String translateMorseCode(List<String> message) {
+		String finalMessage = "";
+		
+		for(int i=0; i<message.size(); i++) {
+			List<String> word = Arrays.asList(message.get(i).split(" "));
+			
+			for(int j=0; j<word.size(); j++) {
+				switch(word.get(j)) {
+				case ".-": 
+					finalMessage += 'A';
+					break;
+				case "-...":
+					finalMessage += 'B';
+					break;
+				case "-.-.":
+					finalMessage += 'C';
+					break;
+				case "-..":
+					finalMessage += 'D';
+					break;
+				case ".":
+					finalMessage += 'E';
+					break;
+				case "..-.":
+					finalMessage += 'F';
+					break;
+				case "--.":
+					finalMessage += 'G';
+					break;
+				case "....":
+					finalMessage += 'H';
+					break;
+				case "..":
+					finalMessage += 'I';
+					break;
+				case ".---":
+					finalMessage += 'J';
+					break;
+				case "-.-":
+					finalMessage += 'K';
+					break;
+				case ".-..":
+					finalMessage += 'L';
+					break;
+				case "--":
+					finalMessage += 'M';
+					break;
+				case "-.":
+					finalMessage += 'N';
+					break;
+				case "---":
+					finalMessage += 'O';
+					break;
+				case ".--.":
+					finalMessage += 'P';
+					break;
+				case "--.-":
+					finalMessage += 'Q';
+					break;
+				case ".-.":
+					finalMessage += 'R';
+					break;
+				case "...":
+					finalMessage += 'S';
+					break;
+				case "-":
+					finalMessage += 'T';
+					break;
+				case "..-":
+					finalMessage += 'U';
+					break;
+				case "...-":
+					finalMessage += 'V';
+					break;
+				case ".--":
+					finalMessage += 'W';
+					break;
+				case "-..-":
+					finalMessage += 'X';
+					break;
+				case "-.--":
+					finalMessage += 'Y';
+					break;
+				case "--..":
+					finalMessage += 'Z';
+					break;
+				case "--..--":
+					finalMessage += ',';
+					break;
+				case ".----.":
+					finalMessage += '\'';
+					break;
+				default:
+					finalMessage += word.get(j);
+				}
+			}
+			
+			if(i != message.size() - 1) {
+				finalMessage += " ";
+			}
+		}
+		
+		System.out.println(finalMessage);
+		return finalMessage;
 	}
 }
--- src/_02_More_Algorithms/_00_MoreAndMoreTests.java
+++ src/_02_More_Algorithms/_00_MoreAndMoreTests.java
@@ -9,53 +9,80 @@ import org.junit.Test;
  * **/
 
 public class _00_MoreAndMoreTests {
+	
+	String multiply(int numOne, int numTwo) {
+		int product = numOne * numTwo;
+		return numOne + " x " + numTwo + " = " + product;
+	}
 
-//	@Test
-//	  public void MultTest() {
-//
-//	    assertEquals("10 x 0 = 0", multiply(10, 0));
-//	    assertEquals("10 x 10 = 100", multiply(10, 10));
-//	    assertEquals("8 x 11 = 88", multiply(8, 11));
-//	  }
-//
-//	@Test
-//	  public void PrimeTest() {
-//
-//	    assertTrue(isPrime(3));
-//	    assertTrue(isPrime(5));
-//	    assertTrue(isPrime(541));
-//	    assertFalse(isPrime(4));
-//	    assertFalse(isPrime(12));
-//	    assertFalse(isPrime(527));
-//
-//	  }
-//
-//	@Test
-//	  public void SquareTest() {
-//
-//	    assertTrue(isSquare(4));
-//	    assertTrue(isSquare(144));
-//	    assertTrue(isSquare(64));
-//	    assertTrue(isSquare(10201));
-//	    assertTrue(isSquare(1));
-//	    assertFalse(isSquare(3));
-//	    assertFalse(isSquare(22));
-//	    assertFalse(isSquare(143));
-//
-//	  }
-//
-//	@Test
-//	  public void CubeTest() {
-//
-//	    assertTrue(isCube(27));
-//	    assertTrue(isCube(216));
-//	    assertTrue(isCube(729));
-//	    assertTrue(isCube(1));
-//	    assertFalse(isCube(3));
-//	    assertFalse(isCube(22));
-//	    assertFalse(isCube(143));
-//
-//	  }
+	@Test
+	  public void MultTest() {
+
+	    assertEquals("10 x 0 = 0", multiply(10, 0));
+	    assertEquals("10 x 10 = 100", multiply(10, 10));
+	    assertEquals("8 x 11 = 88", multiply(8, 11));
+	  }
+	
+	boolean isPrime(int num) {
+		int numHalf = num/2;
+		
+		for(int i=2; i<=numHalf; i++) {
+			if(num % i == 0) {
+				return false;
+			}
+		}
+		
+		return true;
+	}
+
+	@Test
+	  public void PrimeTest() {
+
+	    assertTrue(isPrime(3));
+	    assertTrue(isPrime(5));
+	    assertTrue(isPrime(541));
+	    assertFalse(isPrime(4));
+	    assertFalse(isPrime(12));
+	    assertFalse(isPrime(527));
+
+	  }
+	
+	boolean isSquare(int num) {
+		double sqrt = Math.sqrt(num);
+		return (sqrt - Math.floor(sqrt) == 0);
+	}
+
+	@Test
+	  public void SquareTest() {
+
+	    assertTrue(isSquare(4));
+	    assertTrue(isSquare(144));
+	    assertTrue(isSquare(64));
+	    assertTrue(isSquare(10201));
+	    assertTrue(isSquare(1));
+	    assertFalse(isSquare(3));
+	    assertFalse(isSquare(22));
+	    assertFalse(isSquare(143));
+
+	  }
+	
+	boolean isCube(int num) {
+		double cbrt = Math.cbrt(num);	
+		return (cbrt - Math.floor(cbrt) == 0);
+	}
+
+	@Test
+	  public void CubeTest() {
+
+	    assertTrue(isCube(27));
+	    assertTrue(isCube(216));
+	    assertTrue(isCube(729));
+	    assertTrue(isCube(1));
+	    assertFalse(isCube(3));
+	    assertFalse(isCube(22));
+	    assertFalse(isCube(143));
+
+	  }
 
 
 
